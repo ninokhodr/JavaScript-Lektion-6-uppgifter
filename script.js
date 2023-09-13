667e1871b8af801e5fdf90c8c2e8abf901a547fb
ninokhodr <rudie.khodr@gmail.com>
2023-09-13 17:52:36 +0200

    Added Variables

---
+++ script.js
@@ -0,0 +1,11 @@
+var heltal = 5;
+var decimal = 5.5;
+var string = "Nino"
+var sant = true
+var falskt = false
+
+console.log(typeof heltal);
+console.log(typeof decimal);
+console.log(typeof string);
+console.log(typeof sant);
+console.log(typeof falskt);
